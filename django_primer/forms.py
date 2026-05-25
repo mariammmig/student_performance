37a9fdc6ddd0a5321dfb9967b64c342cf9eb1c64
Maria Migal <marika.migal@gmil.com>
2026-05-25 18:10:19 +0700

    Add Russian labels to StudentForm and ScoreForm

--- django_primer/forms.py
+++ django_primer/forms.py
@@ -5,6 +5,11 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'surname', 'email']
+        labels = {
+            'name': 'Имя',
+            'surname': 'Фамилия',
+            'email': 'Email',
+        }
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}),
             'surname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}),
@@ -15,6 +20,11 @@ class ScoreForm(forms.ModelForm):
     class Meta:
         model = Score
         fields = ['student', 'subject', 'value']
+        labels = {
+            'student': 'Студент',
+            'subject': 'Предмет',
+            'value': 'Оценка',
+        }
         widgets = {
             'student': forms.Select(attrs={'class': 'form-control'}),
             'subject': forms.Select(attrs={'class': 'form-control'}),
